535b1c7385f0ba31cf44914a5f396acf8a8bac58
olivierchaput <68881297+olivierchaput@users.noreply.github.com>
2022-08-10 08:41:39 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+"""App that returns the title and href of games that are free for a limited time on steam"""
+
+
+from lxml import html
+import requests
+
+url = 'https://store.steampowered.com/search/?maxprice=free&specials=1'
+resp = requests.get(url)
+
+# Error messages
+if resp.status_code == 200 :
+    print('\nSuccesfully reached Steam (' + str(resp.status_code) + ')')
+else :
+    print('\nThere was an error in the request')
+
+
+tree = html.fromstring(resp.content)
+elements = tree.xpath('//*[@id="search_resultsRows"]/a/div[2]/div[1]/span')
+
+
+for element in elements:
+    print('----------------------------------')
+    print('Game title : ' + element.text)
+    print('Game URL : ' + element.getparent().getparent().getparent().attrib['href'] + '\n\n')
+
+
